25768b3a5e854a0b39dd350966741639eaadfff1
jldupont <github@jldupont.com>
2011-08-12 22:16:37 -0400

    gs

--- src/cs_grooveshark.js
+++ src/cs_grooveshark.js
@@ -8,6 +8,10 @@
  * 
  **/
 
+var last_song=null;
+var last_artist=null;
+var last_album=null;
+
 function getDetails(){
 	return document.getElementById('playerDetails_nowPlaying');
 };
@@ -20,13 +24,25 @@ details.addEventListener('DOMSubtreeModified', function(evt){
 	var details=getDetails();
 	
 	var songs=getElementsByClassName("song", "a", details);
-	var current_song=songs[0].getAttribute('title');
+	var song=songs[0] || {};
+	var current_song=song.getAttribute && song.getAttribute('title');
 	
 	var artists=getElementsByClassName("artist", "a", details);
-	var current_artist=artists[0].getAttribute('title');
+	var artist=artists[0] || {};
+	var current_artist=artist.getAttribute && artist.getAttribute('title');
 
 	var albums=getElementsByClassName("album", "a", details);
-	var current_album=artists[0].getAttribute('title');
+	var album=albums[0] || {};
+	var current_album=album.getAttribute && album.getAttribute('title');
+	
+	if (current_song==undefined) return;
+	if (current_artist==undefined) return;
+	if (current_album==undefined) return;
+	
+	if ((last_song==current_song) && (last_album==current_album) && (last_artist==current_artist)) return;
+	last_song=current_song;
+	last_artist=current_artist;
+	last_album=current_album;
 	
 	sendMsg('current_track', 'gs', {
 		'song': current_song,
