d3ffdb7cb0e2c9455fac54944340cd021e247926
jldupont <github@jldupont.com>
2011-08-13 16:26:02 -0400

    rdio

--- src/cs_rdio.js
+++ src/cs_rdio.js
@@ -1,10 +1,9 @@
 /**
- * Content script for Grooveshark 
+ * Content script for Rdio.com 
  * 
  *
  * @author Jean-Lou Dupont
  * 
- * id='playerDetails_nowPlaying'
  * 
  * API:
  *   Messaging from extension: msg.mtype= [previous | next | play-pause]
@@ -16,8 +15,17 @@ var last_artist=null;
 var last_album=null;
 var last_state=null;
 
+function getNodeValue(node) {
+	var nodeEl=document.getElementById(node);
+	//console.log(nodeEl);
+	var nodeA=nodeEl.getElementsByTagName("a")[0];
+	//console.log(nodeA);
+	return nodeA.firstChild && nodeA.firstChild.data;	
+};
+
 function getDetails(){
-	return document.getElementById('playerDetails_nowPlaying');
+	var details=getElementsByClassName("player_now_playing_info")[0];
+	return details;
 };
 
 var details=getDetails();
@@ -29,23 +37,19 @@ var details=getDetails();
 details.addEventListener('DOMSubtreeModified', function(evt){
 	//console.log(evt);
 	
-	var details=getDetails();
-	
-	var songs=getElementsByClassName("song", "a", details);
-	var song=songs[0] || {};
-	var current_song=song.getAttribute && song.getAttribute('title');
-	
-	var artists=getElementsByClassName("artist", "a", details);
-	var artist=artists[0] || {};
-	var current_artist=artist.getAttribute && artist.getAttribute('title');
-
-	var albums=getElementsByClassName("album", "a", details);
-	var album=albums[0] || {};
-	var current_album=album.getAttribute && album.getAttribute('title');
+	var current_song=getNodeValue("playerNowPlayingTitle");	
+	var current_artist=getNodeValue("playerNowPlayingArtist");
+	var current_album=getNodeValue("playerNowPlayingAlbum");
 	
 	if (current_song==undefined) return;
 	if (current_artist==undefined) return;
 	if (current_album==undefined) return;
+
+	if (current_song==null) return;
+	if (current_artist==null) return;
+	if (current_album==null) return;
+	
+	console.log(current_song+", "+current_artist+", "+current_album);
 	
 	if ((last_song==current_song) && (last_album==current_album) && (last_artist==current_artist)) return;
 	last_song=current_song;
@@ -53,43 +57,24 @@ details.addEventListener('DOMSubtreeModified', function(evt){
 	last_album=current_album;
 	
 	// report back to the extension
-	sendMsg('current_track', 'gs', {
+	sendMsg('current_track', 'rdio', {
 		'song': current_song,
 		'artist': current_artist,
 		'album': current_album
 	});
 });
 
-var button_play_pause=document.getElementById("player_play_pause");
-button_play_pause.addEventListener('DOMSubtreeModified', function(evt){
-	var classe=evt.target.className.toLowerCase();
-	var bits=classe.split(' ');
-	var state='disabled';
-	var inverted_state=bits[1] || "disabled";
-	
-	if (inverted_state=='pause') state='play';
-	if (inverted_state=='play')  state='pause';
-	//console.log("Play/Pause state: "+state);
-	
-	if (state!=last_state) {
-		sendMsg('current_state', 'gs', {
-			'state': state
-		});
-	}
-	last_state=state;
-});
-
 
 function doPreviousSong() {
-	simulateClick("player_previous");
+	simulateClick("previousButton");
 };
 
 function doNextSong() {
-	simulateClick("player_next");
+	simulateClick("nextButton");
 };
 
 function doPlayPause() {
-	simulateClick("player_play_pause");
+	simulateClick("playButton");
 };
 
 /**
