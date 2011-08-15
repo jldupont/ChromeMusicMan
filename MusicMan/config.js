2e15d23f0ba8d44365d2e512e445a7e903eaaeb6
jldupont <github@jldupont.com>
2011-08-14 21:37:07 -0400

    started pubnub interface

--- MusicMan/config.js
+++ MusicMan/config.js
@@ -9,17 +9,6 @@
  * 
  */
 
-function body_loaded() {
-	
-	var button=$("buttonApply");
-	button.addEventListener("click", handle_apply_button_click);
-	
-	var table=$("keys");
-	
-	table.addEventListener("DOMSubtreeModified", handle_keys_change);
-	
-};
-
 function handle_keys_change(event) {
 
 	//var pubkey=$("pubkey");
@@ -27,6 +16,7 @@ function handle_keys_change(event) {
 	//var seckey=$("seckey");
 	
 	console.log("Key(s) change!");
+	set_apply_button_state(false);
 };
 
 function handle_apply_button_click(evt) {
@@ -38,3 +28,20 @@ function set_apply_button_state(state) {
 	var button=$("buttonApply");
 	button.disabled=state;
 };
+
+
+function body_loaded() {
+	
+	var button=$("buttonApply");
+	button.addEventListener("click", handle_apply_button_click);
+	
+	var pubkey=$("pubkey");
+	pubkey.addEventListener("change", handle_keys_change);
+	
+	var subkey=$("subkey");
+	subkey.addEventListener("change", handle_keys_change);
+	
+	var seckey=$("seckey");
+	seckey.addEventListener("change", handle_keys_change);
+};
+
