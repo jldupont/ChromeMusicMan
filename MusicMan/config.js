27c883c77985b3bdf98332758c4d2e9d1818d5e5
jldupont <github@jldupont.com>
2011-08-14 21:48:15 -0400

    started pubnub interface

--- MusicMan/config.js
+++ MusicMan/config.js
@@ -16,20 +16,39 @@ function handle_keys_change(event) {
 	//var seckey=$("seckey");
 	
 	console.log("Key(s) change!");
-	set_apply_button_state(false);
+	set_apply_button_disable_state(false);
 };
 
 function handle_apply_button_click(evt) {
 
+	var pubkey=$("pubkey").value;
+	var subkey=$("subkey").value;
+	var seckey=$("seckey").value;
+
+	set_apply_button_disable_state(true);
+	
+	chrome.extension.sendRequest({
+		"mtype": "pubnub_keys"
+		,"pubkey": pubkey
+		,"subkey": subkey,
+		,"seckey": seckey
+		
+	}, function(response) {
+		  console.log(response);
+		});	
 };
 
-function set_apply_button_state(state) {
+function set_apply_button_disable_state(state) {
 	
 	var button=$("buttonApply");
 	button.disabled=state;
 };
 
-
+/**
+ * Hook-up all the listeners
+ * 
+ * @return
+ */
 function body_loaded() {
 	
 	var button=$("buttonApply");
