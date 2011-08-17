d19664e659512349341370b5efd93f6c70993e4a
jldupont <github@jldupont.com>
2011-08-17 07:39:46 -0400

    mswitch

--- MusicMan/agents/announcer.js
+++ MusicMan/agents/announcer.js
@@ -7,12 +7,14 @@
  * @dependencies
  * 		- oo.js
  * 		- util2.js
+ * 		- pubnub.js
  * 
  * @author Jean-Lou Dupont
  */
 
 aAnnouncer=new Agent("Announcer");
 
+aAnnouncer=new PubNub();
 aAnnouncer.per_second=2;  //cycles per second
 aAnnouncer.max_backoff=8; //seconds
 aAnnouncer.max_retries=3;
@@ -61,19 +63,36 @@ aAnnouncer.announce = function(){
 	var self=this;
 	
 	each(this.sources, function(source_name, msg){
+		
+		if (msg.inprogress)
+			return;
+		
 		console.log("announce, source("+source_name+"): msg: "+msg);
-		self.sources[source_name].retries--;
-		if (self.sources[source_name].retries==0)
+		
+		msg.retries--;
+		if (msg.retries==0) {
 			toDelete.push(source_name);
+		} else {
+			if (msg.wait>0) {
+				msg.wait--;
+			}
+			if (msg.wait==0) {
+				this.doPublish(source_name, msg);
+			}
+		};
 	});
+	
 	each(toDelete, function(source_name){
 		console.log("Deleting source("+source_name+")");
 		delete self.sources[source_name];
 	});
 };
 
-
-aAnnouncer.doPublish = function(){
+/*
+ * PubNub.method("publish", function(msg, onsuccess, onerror)
+ */
+aAnnouncer.doPublish = function(source_name, msg){
+	msg.inprogress=true;
 	
 };
 
---
+++ MusicMan/mswitch.js
@@ -0,0 +1,24 @@
+/*
+ * Message Switch
+ * 
+ * @dependencies:
+ * 	- oo.js
+ * 
+ * @author Jean-Lou Dupont
+ */
+
+function MSwitch() {
+
+	// by mtype
+	this.subscribers={};
+};
+
+mswitch.method("subscribe", function(){
+	
+});
+
+mswitch.method("publish", function(){
+	
+});
+
+mswitch=new MSwitch();
