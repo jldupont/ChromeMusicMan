3be4d91155e3b42e34fa08813f91a72a42b30322
jldupont <github@jldupont.com>
2011-08-17 07:53:39 -0400

    mswitch

--- MusicMan/mswitch.js
+++ MusicMan/mswitch.js
@@ -3,22 +3,44 @@
  * 
  * @dependencies:
  * 	- oo.js
+ *  - util2.js  ('each')
  * 
  * @author Jean-Lou Dupont
  */
 
 function MSwitch() {
 
+	// all
+	this.subscribers=[];
+	
 	// by mtype
-	this.subscribers={};
+	this.subscribe_map_interested={};
+	this.subscribe_map_not_interested={};
+	
+	// agents by name
+	this.agent_map={};
 };
 
-mswitch.method("subscribe", function(){
-	
+/*
+ * All Agents subscribe but each need to
+ *  reply to a 'publish' with their "interest" 
+ *  in a message type
+ *  
+ *  agent=[name, scope, fn]
+ */
+mswitch.method("subscribe", function(agent){
+	this.subscribers.push(agent);
 });
 
-mswitch.method("publish", function(){
+mswitch.method("publish", function(msg){
+	if (msg.type===undefined) {
+		console.warn("mswitch.publish: type is undefined");
+		return;
+	};
 	
+	each(this.subscribers, function(agent, index){
+		
+	});
 });
 
 mswitch=new MSwitch();
